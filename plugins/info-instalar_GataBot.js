7a9cefa5b7763ee65396ce1bf24f6f522294e1a3
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-13 08:30:53 +0100

    〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕

--- plugins/info-instalar_GataBot.js
+++ plugins/info-instalar_GataBot.js
@@ -1,21 +1,12 @@
-let handler  = async (m, { conn, usedPrefix, command }) => {
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-}
+const handler = async (m, {conn}) => {
+  m.reply(global.ComprarBot);
+};
+handler.command = /^(ComprarBot)$/i;
+export default handler;
+global.ComprarBot = `
+〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕
 
-let picture = './media/menus/Menu1.jpg'
-let gata = `*COMPRAR BOT OPTIMUS*
+*COMPRAR BOT OPTIMUS*
 
 *━━━━━━━━━━━━━⬣*
 *✅ METODO DIAMANTES*
@@ -29,10 +20,5 @@ let gata = `*COMPRAR BOT OPTIMUS*
 *⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
 *YAPE 1 :* 926 933 818 , Luis Alberto , 25 SOLES PROPIO
 *YAPE 2 :* 976 336 831 , Mary Luz , 25 SOLES PROPIO
-*━━━━━━━━━━━━━⬣*`
-await conn.sendFile(m.chat, picture, 'gata.mp4', gata, fkontak)}
-/*conn.sendButton(m.chat, gata, `Comunícate con Mí Creadora si necesitas ayuda con la Instalación.\n\nContact My Creator if you need help with the Installation.\n\n${ig}\n${wm}`, picture, [
-['𝘾𝙪𝙚𝙣𝙩𝙖𝙨 𝙊𝙛𝙞𝙘𝙞𝙖𝙡𝙚𝙨 | 𝘼𝙘𝙘𝙤𝙪𝙣𝙩𝙨 ✅', '.cuentasgb'],
-['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar']], fkontak, m)}*/
-handler.command = /^(comprarbot)/i
-export default handler
+*━━━━━━━━━━━━━⬣*
+`;
