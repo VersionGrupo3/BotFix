aad507c73466771f5d9f65219efc57e130fcc0ec
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-14 08:33:14 +0100

    〔 𝗢𝗽𝘁𝗶𝗺𝘂𝘀 𝗕𝗼𝘁 〕

--- plugins/spam.js
+++ plugins/spam.js
@@ -1,8 +1,8 @@
 const handler = async (m, {conn}) => {
-  m.reply(global.SpamLink);
+  m.reply(global.SpamApk);
 };
 handler.command = /^(SpamApk)$/i;
 export default handler;
-global.SpamLink = `
+global.SpamApk = `
 𝐋𝐈𝐍𝐊 𝐒𝐏𝐀𝐌 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 : https://www.mediafire.com/file/elvm4t2m0tf25ux/SPAM_WHATSAPP.apk/file
 `;
