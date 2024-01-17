cf35edf5808c370519b88c6ab6470bb2ba0b83e7
FantaBot1 <155099912+FantaBot1@users.noreply.github.com>
2024-01-17 14:31:33 +0100

    Update a-MascInterna4.js

--- plugins/a-MascInterna4.js
+++ plugins/a-MascInterna4.js
@@ -11,20 +11,20 @@ global.MascInterna4 = `
 ╭┈┈ ๑❀๑ •  *MASC* • ๑❀๑ ••: 
 │
 │      ෆ _Encargado:_   
-│      ෆ _Horario:_ ( 10 )🇵🇪 ( 00 )🇦🇷 
+│      ෆ _Horario:_ (  )🇵🇪 (  )🇦🇷 
 │      ෆ _Mod:_  
 │ 
 │ㅤㅤ_ʚ Escuadra 1:_ 
-│ㅤ🥷🏻• TH  
-│ㅤ🥷🏻• TH  
-│ㅤ🥷🏻• TH  
-│ㅤ🥷🏻• TH  
+│ㅤ🥷🏻• 
+│ㅤ🥷🏻• 
+│ㅤ🥷🏻• 
+│ㅤ🥷🏻• 
 │
 │ㅤㅤ_ʚ Escuadra 2:_ 
-│ㅤ🥷🏻• TH  
-│ㅤ🥷🏻• TH  
-│  🥷🏻• TH  
-│ㅤ🥷🏻• TH   
+│ㅤ🥷🏻• 
+│ㅤ🥷🏻• 
+│  🥷🏻• 
+│ㅤ🥷🏻• 
 │ 
 │   _⊹ ִֶָ Donador/Donadora De Sala:_  
 │ 
