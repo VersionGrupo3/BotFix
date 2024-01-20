ff17d479aba15b66abdff3a324c0c7a0272f3fd2
VersionGrupo3 <154380259+VersionGrupo3@users.noreply.github.com>
2024-01-20 14:43:51 +0100

    *

--- plugins/info-instalar_GataBot.js
+++ plugins/info-instalar_GataBot.js
@@ -18,8 +18,8 @@ global.ComprarBot = `
 *━━━━━━━━━━━━━⬣*
 *✅ METODO PERU BOT PROPIO*
 *⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
-*YAPE 1 :* 926 933 818 , Luis Alberto , 25 SOLES PROPIO
-*YAPE 2 :* 976 336 831 , Mary Luz , 25 SOLES PROPIO
+*YAPE 1 :* 926 933 818 , Luis Alberto , 20 SOLES PROPIO
+*YAPE 2 :* 976 336 831 , Mary Luz , 20 SOLES PROPIO
 *━━━━━━━━━━━━━⬣*
 
 *━━━━━━━━━━━━━⬣*
@@ -27,5 +27,11 @@ global.ComprarBot = `
 *⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
 *YAPE 1 :* 926 933 818 , Luis Alberto , 5 SOLES 1 GRUPO
 *YAPE 2 :* 976 336 831 , Mary Luz , 5 SOLES 1 GRUPOº
+*━━━━━━━━━━━━━⬣*
+
+*━━━━━━━━━━━━━⬣*
+*✅ WHATSAPP OFICIAL*
+*⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯ ⎯*
+*SI ESTA INTERESADO EN ADQUIRIR EL BOT EL NUMERO ES wa.me/51902360712*
 *━━━━━━━━━━━━━⬣*
 `;
